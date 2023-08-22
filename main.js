250d133fc526de8000363abea75d2a0cfe530db5
iKondor <estebanrojas9314@gmail.com>
2023-08-21 21:00:28 -0300

    CambiosTP3

--- main.js
+++ main.js
@@ -1,108 +1,170 @@
-// Clase para representar un préstamo
+// Definimos una clase llamada 'Prestamo'
 class Prestamo {
-  constructor(monto, tasa, periodos) {
-
-    // Propiedades del préstamo
-    this.monto_prestamo = monto;
-    this.tasa_interes = tasa;
-    this.num_periodos = periodos;
-    this.intereses_acumulados = 0;
-    this.fechas = [];
-    this.intereses = [];
+ 
+  constructor(monto, tasa, periodos, fecha) {
+    this.monto_prestamo = monto; // Monto total del préstamo.
+    this.tasa_interes = tasa / 100; // Tasa de interés expresada como una fracción decimal.
+    this.num_periodos = periodos; // Número de periodos de pago.
+    this.fecha = fecha; // Fecha en que se crea el préstamo.
+    this.intereses_acumulados = 0; // Intereses que se han acumulado hasta ahora.
+    this.cuotas = []; // Una lista vacía para almacenar la información de cada cuota.
   }
 
-  // Método para calcular los intereses acumulados
+  // Método para calcular los intereses acumulados en el préstamo.
   calcularIntereses() {
-
-    // Reiniciar el contador de intereses acumulados
-    this.intereses_acumulados = 0;
-
-    // Calcular los intereses acumulados por cada período
-    for (var periodo = 1; periodo <= this.num_periodos; periodo++) {
-      var intereses_periodo = this.monto_prestamo * (this.tasa_interes / 100);
-      this.intereses_acumulados += intereses_periodo;
-    }
-
-    // Guardar la tasa de interés y la fecha en los arrays
-    this.intereses.push(this.tasa_interes);
-    var fechaInteres = prompt("Ingrese la fecha de los intereses (Formato: DD/MM/AAAA):");
-    this.fechas.push(fechaInteres);
-
-    // Mostrar los intereses acumulados en el resultado del formulario y en la consola
-    document.getElementById("resultado").value = "$" + this.intereses_acumulados.toFixed(2);
-    console.log("El total de intereses acumulados es: $" + this.intereses_acumulados.toFixed(2));
+    this.intereses_acumulados = this.monto_prestamo * this.tasa_interes * this.num_periodos;
   }
 
-  // Método para buscar un interés por fecha
-  buscarInteresPorFecha(fecha) {
-    var interesEncontrado = null;
-    for (var i = 0; i < this.fechas.length; i++) {
-      if (this.fechas[i] === fecha) {
-        interesEncontrado = this.intereses[i];
-        break;
-      }
+  // Método para calcular las cuotas de pago del préstamo.
+  calcularCuotas() {
+    let saldo_pendiente = this.monto_prestamo; // Cantidad que aún queda por pagar del préstamo.
+
+    // La cuota fija es la cantidad que se paga en cada periodo, calculada según la fórmula del préstamo amortizado.
+    let cuota_fija = this.monto_prestamo * (this.tasa_interes / (1 - Math.pow(1 + this.tasa_interes, -this.num_periodos)));
+
+    // Ciclo for que recorre cada periodo del préstamo.
+    for (let periodo = 1; periodo <= this.num_periodos; periodo++) {
+      let intereses_periodo = saldo_pendiente * this.tasa_interes;
+      let cuota_contra_capital = cuota_fija - intereses_periodo;
+
+      saldo_pendiente -= cuota_contra_capital; // Restamos la cuota contra el capital del saldo pendiente.
+
+      // Añadimos la información de la cuota a la lista de cuotas.
+
+      this.cuotas.push({
+        cuota_fija,
+        intereses_periodo,
+        cuota_contra_capital,
+        saldo_pendiente,
+      });
     }
-    return interesEncontrado;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Obtener la lista para mostrar los intereses guardados
-  var interesesGuardadosList = document.getElementById("interesesGuardadosList");
+// Creamos un array vacío para almacenar los préstamos.
+const prestamos = [];
 
-  // Array para almacenar los objetos "Prestamos"
-  var prestamos = [];
+// Función que se ejecuta cuando se presiona el botón de calcular.
+function calculateLoan() {
 
-  // Evento click en el botón "Calcular"
-  document.getElementById("calcularBtn").addEventListener("click", function() {
+  // Obtenemos los datos de los inputs en el HTML.
+  const loanData = {
+    loanAmount: document.getElementById('montoInput').value,
+    interestRate: document.getElementById('tasaInput').value,
+    loanPeriods: document.getElementById('periodosInput').value,
+    loanDate: document.getElementById('fechaInput').value
+  }
 
-    // Obtener los valores ingresados por el usuario
-    var monto_prestamo = parseFloat(document.getElementById("montoInput").value);
-    var tasa_interes = parseFloat(document.getElementById("tasaInput").value);
-    var num_periodos = parseInt(document.getElementById("periodosInput").value);
+  // Desestructuramos loanData en variables separadas.
+  const {loanAmount, interestRate, loanPeriods, loanDate} = loanData;
 
-    // Crear un nuevo objeto "Prestamo" y agregarlo al array
-    var nuevoPrestamo = new Prestamo(monto_prestamo, tasa_interes, num_periodos);
-    prestamos.push(nuevoPrestamo);
+  // Utilizamos el operador ternario para verificar si los campos están llenos
+  // antes de calcular el préstamo.
+  loanAmount && interestRate && loanPeriods && loanDate
+    ? createLoan(parseFloat(loanAmount), parseFloat(interestRate), parseInt(loanPeriods), loanDate)
+    : alert('Por favor llena todos los campos antes de calcular.');
+}
 
-    // Calcular intereses para el nuevo préstamo
-    nuevoPrestamo.calcularIntereses();
+document.addEventListener('DOMContentLoaded', function() {
+  cargarDatosAlmacenados();
+
+  // Obtener referencias a los elementos del DOM
+  const calcularBtn = document.getElementById('calcularBtn');
+  const historialBtn = document.getElementById('historialBtn');
+  const mostrarBtn = document.getElementById('mostrarBtn');
+
+  // Asociar eventos a los botones
+  calcularBtn.addEventListener('click', calculateLoan);
+  historialBtn.addEventListener('click', showHistorial);
+  mostrarBtn.addEventListener('click', showInteresesGuardados);
+
+});
+
+// Función para crear un nuevo préstamo y añadirlo al array de préstamos.
+function createLoan(loanAmount, interestRate, loanPeriods, loanDate) {
+  const prestamo = new Prestamo(loanAmount, interestRate, loanPeriods, loanDate); 
+
+  prestamo.calcularIntereses();
+  prestamo.calcularCuotas();
+  
+  // Almacenar el préstamo en Local Storage
+  prestamos.push(prestamo);
+  localStorage.setItem('prestamos', JSON.stringify(prestamos));
+
+  document.getElementById('resultado').value = prestamo.intereses_acumulados.toFixed(2);
+
+  displayLoanInstallments(prestamo);
+}
+
+// Función para mostrar las cuotas de un préstamo en el HTML.
+function displayLoanInstallments(prestamo) {
+  const detalleCuotas = document.getElementById('detalleCuotas');
+  detalleCuotas.innerHTML = ''; // Limpiamos el contenido previo del div.
+
+  // Recorremos la lista de cuotas del préstamo.
+  prestamo.cuotas.forEach((cuota, index) => {
+  
+    const { cuota_fija, intereses_periodo, cuota_contra_capital, saldo_pendiente } = cuota;
+    const cuotaP = document.createElement('p'); 
+
+    cuotaP.textContent = `Cuota ${index + 1}: $${cuota_fija.toFixed(2)} (Intereses: $${intereses_periodo.toFixed(2)}, Capital: $${cuota_contra_capital.toFixed(2)}, Saldo pendiente: $${saldo_pendiente.toFixed(2)})`;
+
+    // Añadimos el párrafo al div de detalle de cuotas.
+    detalleCuotas.appendChild(cuotaP);
   });
+}
 
-  // Evento click en el botón "Historial de Intereses"
-  document.getElementById("historialBtn").addEventListener("click", function() {
+// Cargamos los préstamos almacenados en el Local Storage al inicio
+function cargarDatosAlmacenados() {
+  const datosGuardados = localStorage.getItem('prestamos');
+  if (datosGuardados) {
+    const prestamosGuardados = JSON.parse(datosGuardados).map(
+      prestamo =>
+        new Prestamo(
+          prestamo.monto_prestamo,
+          prestamo.tasa_interes,
+          prestamo.num_periodos,
+          prestamo.fecha
+        )
+    );
+    prestamos.push(...prestamosGuardados);
+  }
+}
 
-    // Pedir al usuario la fecha de interés a buscar
-    var fechaBusqueda = prompt("Ingrese la fecha de interés a buscar (Formato: DD/MM/AAAA):");
 
-    // Búsqueda de intereses correspondientes a la fecha ingresada en todos los préstamos almacenados
-    var interesEncontrado = null;
-    for (var i = 0; i < prestamos.length; i++) {
-      var prestamo = prestamos[i];
-      interesEncontrado = prestamo.buscarInteresPorFecha(fechaBusqueda);
-      if (interesEncontrado !== null) {
-        break;
-      }
-    }
+document.getElementById('calcularBtn').addEventListener('click', calculateLoan);
 
-    // Mostrar el interés encontrado o un mensaje de no encontrado
-    if (interesEncontrado !== null) {
-      alert("La tasa de interés para la fecha " + fechaBusqueda + " es: " + interesEncontrado + "%");
-    } else {
-      alert("No se encontró ninguna tasa de interés para la fecha " + fechaBusqueda);
-    }
-  });
+document.getElementById('historialBtn').addEventListener('click', () => {
+  const searchDate = document.getElementById('fechaInteres').value; 
+  const resultDiv = document.getElementById('cuotasList');
 
-  // Evento click en el botón "Mostrar Intereses Guardados"
-  document.getElementById("mostrarBtn").addEventListener("click", function() {
-    // Mostrar los intereses guardados para todos los préstamos en una lista
-    interesesGuardadosList.innerHTML = ""; // Limpiar la lista antes de mostrar nuevos datos
+  // Filtramos los préstamos que coinciden con la fecha buscada.
+  const foundLoans = prestamos.filter(loan => loan.fecha === searchDate);
+  resultDiv.innerHTML = '';
 
-    for (var i = 0; i < prestamos.length; i++) {
-      var prestamo = prestamos[i];
-      var interesesGuardados = document.createElement("li");
-      interesesGuardados.textContent = "Fecha: " + prestamo.fechas.join(", ") + " - Porcentaje de interés: " + prestamo.intereses.join("%, ");
-      interesesGuardadosList.appendChild(interesesGuardados);
-    }
+  foundLoans.forEach((loan, index) => {
+    const liElement = document.createElement('li'); 
+    liElement.textContent = `Préstamo ${index+1} - Intereses acumulados: ${loan.intereses_acumulados.toFixed(2)}`;
+
+    // Añadimos el elemento de lista al div de resultados.
+    resultDiv.appendChild(liElement);
   });
 });
+
+// Event listener para el botón de mostrar. Cuando se presione el botón, se mostrarán todos los préstamos en el HTML.
+document.getElementById('mostrarBtn').addEventListener('click', () => {
+  const resultDiv = document.getElementById('interesesGuardadosList'); // Obtenemos el div donde se mostrarán los préstamos.
+
+  resultDiv.innerHTML = ''; // Limpiamos el contenido previo del div.
+
+  // Recorremos la lista de préstamos.
+  prestamos.forEach((loan, index) => {
+    const liElement = document.createElement('li'); 
+    
+    liElement.textContent = `Préstamo ${index+1} - Fecha: ${loan.fecha} - Intereses acumulados: ${loan.intereses_acumulados.toFixed(2)}`;
+
+  
+    resultDiv.appendChild(liElement);
+  });
+});
+
